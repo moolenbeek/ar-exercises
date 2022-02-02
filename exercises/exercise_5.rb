c8f2fa06e1135d375ad3f5b1c383f5defef03a14
Eric Moolenbeek <emoolenbeek@gmail.com>
2022-02-02 01:51:28 +0000

    exercise_5 calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# annual annual_revenue of all stores
+puts Store.sum(:annual_revenue)
+
+# average annual_revenue from all stores
+puts Store.average(:annual_revenue)
+
+# number of stores with an annual_revenue of over 1M
+puts Store.where("annual_revenue > '1000000'").count
