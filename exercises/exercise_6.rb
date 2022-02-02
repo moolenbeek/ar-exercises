2f1a8653777b4191b133a397ce52d42aa75a7074
Eric Moolenbeek <emoolenbeek@gmail.com>
2022-02-02 19:04:44 +0000

    exercise_6 One-to-many association and exercise_7 validation

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Mohamed", last_name: "Mohamed", hourly_rate: 60)
+@store1.employees.create(first_name: "Steve", last_name: "O", hourly_rate: 60)
+@store1.save
+
+@store2.employees.create(first_name: "John", last_name: "Appleseed", hourly_rate: 55)
+@store2.employees.create(first_name: "Ali", last_name: "Furze", hourly_rate: 120)
+@store2.employees.create(first_name: "Alex", last_name: "Berry", hourly_rate: 160)
+@store2.save
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,4 @@
 class Employee < ActiveRecord::Base
-
+  belongs_to :store
+  validates :first_name, :last_name, presence: true
 end
